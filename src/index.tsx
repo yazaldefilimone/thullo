b2d7b2c616790662337143b068a63058ad4e3ba3
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-21 17:18:21 +0200

    feat; create main start

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/presentation/App';
+import { Screen } from '@/main/screens';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Screen />
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
