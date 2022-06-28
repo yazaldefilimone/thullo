fdb39a429f9d4dfeb41765bc6037824f3febfe3b
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-28 13:33:55 +0200

    feat: create header component

--- src/presentation/components/Header/index.tsx
+++ src/presentation/components/Header/index.tsx
@@ -3,34 +3,37 @@ import { FunctionComponent, ReactNode } from 'react';
 import styles from './styles.module.scss';
 import logo from '@/shared/assets/Logo.svg';
 import photo from '@/shared/assets/photo.jpg';
-
 interface HeaderProps {
   children?: ReactNode;
 }
 export const Header: FunctionComponent<HeaderProps> = () => {
   return (
     <header className={styles.header}>
-      <a href="#">
-        <img src={logo} alt="" />
-      </a>
-      <div className={styles.first__container}>
-        <span>Devchallenges Board</span>
-        <span className={styles.separate}></span>
-        <button>All board</button>
-      </div>
-
-      <div className={styles.side__left}>
-        <div className={styles.left__container}>
-          <input type="text" placeholder="Keyword..." />
-          <button>Search</button>
-        </div>
-
-        <div className={styles.profile}>
-          <div className={styles.profile__bg}>
-            <img src={photo} alt="" />
+      <nav className={`${styles.nav} container`}>
+        <div className={styles.first__container}>
+          <a href="#">
+            <img src={logo} alt="" />
+          </a>
+          <div>
+            <span>Devchallenges Board</span>
+            <span className={styles.separate}></span>
+            <button>All board</button>
           </div>
         </div>
-      </div>
+
+        <div className={styles.side__left}>
+          <div className={styles.left__container}>
+            <input type="text" placeholder="Keyword..." />
+            <button>Search</button>
+          </div>
+
+          <div className={styles.profile}>
+            <div className={styles.profile__bg}>
+              <img src={photo} alt="" />
+            </div>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
