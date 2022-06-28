2e54efd8020085a99f5186b360b73c77f14235d7
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-28 16:06:26 +0200

    feat: add nav and board in home page

--- src/presentation/pages/Home/index.tsx
+++ src/presentation/pages/Home/index.tsx
@@ -1,10 +1,16 @@
+import Board from '@/presentation/components/Board';
 import { Header } from '@/presentation/components/Header';
+import Nave from '@/presentation/components/Nave';
 import React, { FunctionComponent } from 'react';
 
 export const Home: FunctionComponent = () => {
   return (
     <article>
       <Header />
+      <main className="container">
+        <Nave />
+        <Board />
+      </main>
     </article>
   );
 };
