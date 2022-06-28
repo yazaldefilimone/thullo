545fbe0290c60e822f6c2990eee7ac1771d481c1
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-28 13:50:46 +0200

    feat: create navegation

---
+++ src/presentation/components/Nave/index.tsx
@@ -0,0 +1,39 @@
+import { FunctionComponent, ReactNode } from 'react';
+import photo from '@/shared/assets/photo.jpg';
+
+import styles from './styles.module.scss';
+
+interface NaveProps {
+  children?: ReactNode;
+}
+
+const Nave: FunctionComponent<NaveProps> = () => {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.content}>
+        <button>Private</button>
+        <div className={styles.user__container}>
+          <ul>
+            <li>
+              <div className={styles.user}>
+                <img src={photo} alt="" />
+              </div>
+            </li>
+            <li>
+              <div className={styles.user}>
+                <img src={photo} alt="" />
+              </div>
+            </li>
+          </ul>
+          <button>+</button>
+        </div>
+      </div>
+
+      <div className={styles.menu}>
+        <button>Show Menu</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Nave;
